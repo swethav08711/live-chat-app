039c92f709d1a7b66ca9ff814002abea22521b51
swethav08711 <shwe08711@gmail.com>
2022-01-19 17:53:09 +0530

    chat app

---
+++ server/index.js
@@ -0,0 +1,32 @@
+//express =>
+//to build sever
+const express = require("express")
+const app = express()
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
+app.use(cors()) //middleware
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+io.on("connection", socket => {
+  console.log(`UserConnected:${socket.id}`)
+  socket.on("join_room", data => {
+    socket.join(data)
+    console.log(`use with id: ${socket.id} joined room:${data}`)
+  })
+  socket.on("send_message", data => {
+    socket.to(data.room).emit("receive_message", data)
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id)
+  })
+})
+server.listen(3001, () => {
+  console.log("SERVER RUNNING")
+})
